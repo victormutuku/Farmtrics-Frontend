aa5535ff65d33c98757e5cccc6d23e4d5d696fed
Victor Mutuku <victor.mutuku@strathmore.edu>
2023-03-09 00:18:06 +0300

    made the pricing card a component

---
+++ resources/views/components/pricing_card.blade.php
@@ -0,0 +1,22 @@
+<div class="hover:rounded-3xl hover:bg-green-50/100 h-[500px] py-5 w-[300px] flex flex-col items-center px-5">
+    @yield('pricingContent')
+</div>
+
+{{-- <div class="hover:rounded-3xl hover:bg-green-50/100 h-[500px] py-5 w-[300px] flex flex-col items-center px-5">
+    <h1 class="font-bold text-xl">Basic</h1>
+    <div class="flex items-center justify-center my-7">
+        <p class="text-forestgreen font-bold text-2xl">$8/month</p>
+    </div>
+        <div class="pl-10">
+            <ul class="list-outside list-disc">
+                <li>4 users max</li>
+                <li class="text-gray-400">Availability of farmer's identities</li>
+            </ul>
+        </div>
+        <div class="relative flex flex-col items-center bottom-[-109px]">
+            <a href="#" class="text-forestgreen font-bold mb-[40px]">Learn More</a>
+            <div class="bg-forestgreen rounded-[14px] drop-shadow-sm hidden md:block">
+                <a href="#" class="py-4 px-10 inline-block text-lg text-white">Sign up</a>
+            </div>
+        </div>
+</div> --}}
--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -17,7 +17,8 @@
         <div>
         </div>
         <div class="flex items-center justify-center mb-10">
-            <div class="hover:rounded-3xl hover:bg-green-50/100 h-[500px] py-5 w-[300px] flex flex-col items-center px-5">
+            
+            @section('pricingContent')
                 <h1 class="font-bold text-xl">Basic</h1>
                 <div class="flex items-center justify-center my-7">
                     <p class="text-forestgreen font-bold text-2xl">$8/month</p>
@@ -34,9 +35,10 @@
                             <a href="#" class="py-4 px-10 inline-block text-lg text-white">Sign up</a>
                         </div>
                     </div>
-            </div>
-            <div class="hover:rounded-3xl hover:bg-green-50/100 h-[500px] px-10 py-5 w-[300px] flex flex-col items-center pr-5">
-                <h1 class="font-bold text-xl">Pro</h1>
+            @endsection
+
+            @section('pricingContent')
+               <h1 class="font-bold text-xl">Pro</h1>
                 <div class="flex items-center justify-center my-7">
                     <p class="text-forestgreen font-bold text-2xl">$20/month</p>
                 </div>
@@ -51,9 +53,10 @@
                     <div class="bg-forestgreen rounded-[14px] drop-shadow-sm hidden md:block">
                         <a href="#" class="py-4 px-10 inline-block text-lg text-white">Sign up</a>
                     </div>
-                </div>
-            </div>
-            <div class="hover:rounded-3xl hover:bg-green-50/100 h-[500px] px-10 py-5 w-[300px] flex flex-col items-center">
+                </div> 
+            @endsection
+            
+            @section('pricingContent')
                 <h1 class="font-bold text-xl">Enterprise</h1>
                 <div class="relative flex flex-col items-center bottom-[-268px]">
                     <a href="#" class="text-forestgreen font-bold mb-[40px]">Learn More</a>
@@ -61,7 +64,7 @@
                         <a href="#" class="py-4 px-10 inline-block text-lg text-white">Contact us</a>
                     </div>
                 </div>
-            </div>
+            @endsection
         </div>
     </div> 
 </div>
