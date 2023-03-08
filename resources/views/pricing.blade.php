f88ea401a3ca13eb857323992154ae2d763ebf50
Victor Mutuku <victor.mutuku@strathmore.edu>
2023-03-08 16:54:35 +0300

    added pricing page

---
+++ resources/views/pricing.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Farmtrics - Pricing</title>
+</head>
+<body class="min-h-full">
+    <!--Content Wrapper-->
+    <div class="h-full"> 
+    
+        <!-- Navigation -->
+        @extends('layouts.navbar')
+        <main>
+            <div class="flex justify-center mt-[200px]">
+                <h1 class="text-forestgreen font-bold text-9xl mb-[200px]">Contact us</h1>
+            </div>
+        </main>
+        @extends('layouts.footer')
+    </div>
+</body>
+</html>
