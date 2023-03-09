08b21cba62e019fdc5233ecefc2c1d5312293872
Victor Mutuku <victor.mutuku@strathmore.edu>
2023-03-09 10:53:26 +0300

    Pricing page restored to previous state and vercel.json fixed

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -8,7 +8,7 @@
     <h1 class="text-forestgreen font-semibold text-5xl mb-[80px]">Pricing for Every Business, at Any Stage</h1>
 </div>
 <div class="relative">
-   <div class="absolute bg-black -inset-0.5 blur opacity-5  mb-[200px] mx-[200px] rounded-[54px]"></div>
+    <div class="absolute bg-black -inset-0.5 blur opacity-5  mb-[200px] mx-[200px] rounded-[54px]"></div>
     <div class="relative bg-white border-2 mb-[200px] mx-[200px] rounded-[34px]">
         <div class="px-10 py-8">
             <h2 class=" font-semibold text-2xl">Estimate your spend</h2>
@@ -18,7 +18,7 @@
         </div>
         <div class="flex items-center justify-center mb-10">
             
-            @section('pricingContent')
+            <div class="hover:rounded-3xl hover:bg-green-50/100 h-[500px] py-5 w-[300px] flex flex-col items-center px-5">
                 <h1 class="font-bold text-xl">Basic</h1>
                 <div class="flex items-center justify-center my-7">
                     <p class="text-forestgreen font-bold text-2xl">$8/month</p>
@@ -35,9 +35,9 @@
                             <a href="#" class="py-4 px-10 inline-block text-lg text-white">Sign up</a>
                         </div>
                     </div>
-            @endsection
+            </div>
 
-            @section('pricingContent')
+            <div class="hover:rounded-3xl hover:bg-green-50/100 h-[500px] py-5 w-[300px] flex flex-col items-center px-5">
                <h1 class="font-bold text-xl">Pro</h1>
                 <div class="flex items-center justify-center my-7">
                     <p class="text-forestgreen font-bold text-2xl">$20/month</p>
@@ -54,9 +54,9 @@
                         <a href="#" class="py-4 px-10 inline-block text-lg text-white">Sign up</a>
                     </div>
                 </div> 
-            @endsection
+            </div>
             
-            @section('pricingContent')
+            <div class="hover:rounded-3xl hover:bg-green-50/100 h-[500px] py-5 w-[300px] flex flex-col items-center px-5">
                 <h1 class="font-bold text-xl">Enterprise</h1>
                 <div class="relative flex flex-col items-center bottom-[-268px]">
                     <a href="#" class="text-forestgreen font-bold mb-[40px]">Learn More</a>
@@ -64,7 +64,7 @@
                         <a href="#" class="py-4 px-10 inline-block text-lg text-white">Contact us</a>
                     </div>
                 </div>
-            @endsection
+            </div>
         </div>
     </div> 
 </div>
