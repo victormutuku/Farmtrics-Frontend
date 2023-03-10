0e062d7c7e14c5ce3db89ced37b03ea04d2e35f2
Victor Mutuku <victor.mutuku@strathmore.edu>
2023-03-10 13:38:56 +0300

    Changed package name

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -31,7 +31,7 @@
         <div class="flex items-center justify-center mb-10">
             
             <div class="hover:rounded-3xl hover:bg-green-50/100 h-[500px] py-5 w-[300px] flex flex-col items-center px-5">
-                <h1 class="font-bold text-xl">Basic</h1>
+                <h1 class="font-bold text-xl">Starter</h1>
                 <div class="flex items-center justify-center my-7">
                     <p class="text-forestgreen font-bold text-2xl">$8/month</p>
                 </div>
