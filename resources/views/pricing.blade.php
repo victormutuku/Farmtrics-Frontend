9ebffccd3e38fc4411a6af0b7dcfcbf205029cf0
Victor Mutuku <victor.mutuku@strathmore.edu>
2023-03-10 20:10:19 +0300

    Populated enterprise card and correct error in pro card

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -56,7 +56,7 @@
                 </div>
                 <div class="pl-10">
                     <ul class="list-outside list-disc">
-                        <li>4 users max</li>
+                        <li>15 users max</li>
                         <li>Availability of farmer's identities</li>
                     </ul>
                 </div>
@@ -70,7 +70,8 @@
             
             <div class="hover:rounded-3xl hover:bg-green-100 h-[500px] py-5 w-[300px] flex flex-col items-center px-5">
                 <h1 class="font-bold text-xl">Enterprise</h1>
-                <div class="relative flex flex-col items-center bottom-[-268px]">
+                <div class="mt-[86px]"><p class="font-semibold text-2xl text-center">Contact<br>Sales</p></div>
+                <div class="relative flex flex-col items-center bottom-[-124px]">
                     <a href="#" class="text-forestgreen font-bold mb-[40px]">Learn More</a>
                     <div class="bg-forestgreen rounded-[14px] drop-shadow-sm hidden md:block">
                         <a href="#" class="py-4 px-10 inline-block text-lg text-white">Contact us</a>
