37a58001e22ac78654dd37361c7af04a95efa857
Victor Mutuku <victor.mutuku@strathmore.edu>
2023-03-16 23:59:29 +0300

    Populated Login Page : Not Complete

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,54 @@
-@extends('layouts.layout')
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link 
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"/>
+        @vite('resources/css/app.css')
+        <title>Sign In | Farmtrics</title>
+    </head>
+    <body class="bg-green-100">
+    <div class="p-5"> 
+        <a href="/"><i class="fa-arrow-left fa-solid fa-xl"></i></a>
+    </div>
+        <div class="mt-[100px]">
+            <div class="flex justify-center mb-[200px] items-center">
+                <div class="w-[470px] h-[518px] rounded-2xl border-2 border-forestgreen"></div>
+                <div class="flex flex-col items-center ml-[80px] ">
+                    <h1 class="text-forestgreen text-6xl font-medium mb-16">Login</h1>
+                    <form action="#" method="POST" class="w-[470px] flex flex-col items-center">
+                        
+                        <input placeholder="Email" type="email" name="email" class="border-none placeholder:text-forestgreen mb-6 border-2 py-4 pl-4 rounded-2xl w-full focus:outline-none focus:border-forestgreen " required>
+                        
+                        <input placeholder="Password" type="password" name="password" class="border-none placeholder:text-forestgreen mb-6 border-2 py-4 pl-4 rounded-2xl w-full focus:outline-none focus:border-forestgreen" required>
+                        
+                        <input placeholder="Confirm Password" id="cpp" type="confirmPassword" name="confirmPassword" class="border-none placeholder:text-forestgreen mb-6 border-2 py-4 pl-4 rounded-2xl w-full focus:outline-none focus:border-forestgreen" required>           
 
-@section('title', 'Sign In')
+                        <button  type="submit" class="bg-forestgreen text-white py-4 px-10 text-lg rounded-[14px]">Login</button>
 
-@section('content')
+                        <a href="#" class="mt-5"> Don't have an account? <span class="underline">Create One</span></a>
+                    </form>
+                </div>
+                
+            
+            </div> 
+        </div>
+        
+</body>
+</html>
 
-@endsection
+{{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script> --}}
+
+        <script>
+            $(document).ready(function(){
+            $("span").click(function(){
+                $("cpp").toggle();
+            });
+            });
+        </script>
