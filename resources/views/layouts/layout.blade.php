71624b0ed4c1396b3fdf8617dc40a4ac41b67b76
Victor Mutuku <victor.mutuku@strathmore.edu>
2023-06-24 07:32:15 +0300

    Added layouts

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,20 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link  href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css"   rel="stylesheet" type="text/css" />
+    @vite('resources/css/app.css')
     <title>@yield('title') | Farmtrics</title>
+
 </head>
-<body class="min-h-full bg-green-100">
+<body class="min-h-full bg-white">
 
     <div class="h-full">
-
         @extends('layouts.navbar')
-
         <main>
             @yield('content')
         </main>
         
         @extends('layouts.footer')
     </div>
-    
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+ <!-- This is for toaster  -->
+ <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+ <script>
+ @if (Session::has('message'))
+ var type = "{{Session::get('alert-type','info')}}"
+ switch(type){
+   case 'info':
+   toastr.info("{{Session::get('message')}} ");
+   break;
+   case 'success':
+   toastr.success("{{Session::get('message')}} ");
+   break;
+   case 'warning':
+   toastr.warning("{{Session::get('message')}} ");
+   break;
+   case 'error':
+   toastr.error("{{Session::get('message')}} ");
+   break;
+ }
+ 
+ @endif
+ </script>
 </body>
 </html>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,27 +3,30 @@
     <head>
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
-        @vite('resources/css/app.css')
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		@vite('resources/css/app.css')
     </head>
     <header class=" h-20 fixed w-full top-0 z-20"> 
 
-        <nav class="flex h-full md:justify-around justify-between items-center relative w-full lg:bg-green-100">
+        <nav class="flex h-full md:justify-around justify-between items-center relative w-full bg-white">
             <!-- Mobile Nav as -->
             <a 
                 id="mobile-open-a" 
-                class="text-4xl focus:outline-none lg:hidden
-                sm:pl-4 cursor-pointer"> &#9776</a>
+                class="w-[50px] text-4xl focus:outline-none lg:hidden sm:pl-4 cursor-pointer"> &#9776</a>
             <a 
                 id="mobile-close-a" 
-                class="hidden text-6xl focus:outline-none lg:hidden
-                sm:pl-4 cursor-pointer">&times;</a>
+                class="w-[50px] hidden text-6xl focus:outline-none lg:hidden sm:pl-4 cursor-pointer">&times;</a>
             <!-- End of Mobile Nav as -->
 
-            <div class="relative ">
-                <h1 class="text-3xl font-bold">
-                    <a href="/">Farmtrics </a>
-                </h1>
+            <div class="relative">
+					<a href="/"class="flex" >
+						<div class="w-[25px] h-[25px] mt-[8px] mr-1 bg-[url('../../public/fm-logo-3.svg')] bg-contain bg-no-repeat"></div>
+						<div>
+                            <p class="text-3xl font-bold">Farmtrics</p>
+                            <p class="-mt-2 text-[0.6rem] font-semibold text-right">data driven decisions</p>
+                        </div>
+                        
+					</a>
             </div>
 
             <!--Web Navbar-->
@@ -34,10 +37,7 @@
                 <div class="bg-forestgreen rounded-[14px] hidden md:block">
                     <a href="/login" class="px-10 py-3 inline-block text-white">{{ __('navbar.Login') }}</a>
                 </div>
-                <select aria-label="Select Language" class="form-control changeLang cursor-pointer bg-transparent lg:bg-green-100 md:bg-transparent sm:bg-transparent px-2 py-2 shadow-none" name="language-selection">
-                    <option class="hover:bg-green-100" value="en"{{ session()->get('locale') =='en' ? 'selected' : '' }}>{{ __('navbar.English') }}</option>
-                    <option class="hover:bg-green-100" value="fr"{{ session()->get('locale') =='fr' ? 'selected' : '' }}>{{ __('navbar.French') }}</option>
-                </select>
+                <x-navbar.lang-select/>
             </div>
 
         </nav>
@@ -47,75 +47,6 @@
         <!--End of Mobile nav menu-->
     </header> 
 </html>
-
-<script>
-	//Selecting Elements from the DOM
-	const navOpenBtn = document.querySelector('#mobile-open-a');
-	const navCloseBtn = document.querySelector('#mobile-close-a');
-	const mobNavContainer = document.querySelector('#mobile-nav-container');
-	const chevronDown = document.querySelectorAll('.fa-chevron-down');
-	const chevronUp = document.querySelectorAll('.fa-chevron-up');
-	const chevronParent = document.querySelectorAll('.chev-parent');
-
-
-	//Hamburger Menu functionality
-
-	const hamburgerMenu = function () {
-		navOpenBtn.addEventListener('click', function (e) {
-			mobNavContainer.classList.remove('-translate-x-full');
-			navCloseBtn.classList.remove('hidden');
-			navOpenBtn.classList.add('hidden');
-		});
-
-		navCloseBtn.addEventListener('click', function (e) {
-			mobNavContainer.classList.add('-translate-x-full');
-			navCloseBtn.classList.add('hidden');
-			navOpenBtn.classList.remove('hidden');
-		});
-	};
-
-	hamburgerMenu();
-
-	//Dropdown Menu functionality
-	const dropdownMenu = function () {
-		chevronParent.forEach(function (el) {
-			el.addEventListener('click', function (e) {
-				document.querySelectorAll('.ul-hidden').forEach(function (el) {
-					el.classList.add('hidden');
-					document
-						.querySelectorAll('.fa-chevron-down')
-						.forEach(function (el) {
-							el.classList.remove('hidden');
-						});
-
-					document
-						.querySelectorAll('.fa-chevron-up')
-						.forEach(function (el) {
-							el.classList.add('hidden');
-						});
-				});
-
-				if (e.target.classList.contains('fa-chevron-down')) {
-					e.target.nextElementSibling.classList.remove('hidden');
-					e.target.classList.add('hidden');
-					e.target
-						.closest('a')
-						.nextElementSibling.classList.remove('hidden');
-				}
-
-				if (e.target.classList.contains('fa-chevron-up')) {
-					e.target.previousElementSibling.classList.remove('hidden');
-					e.target.classList.add('hidden');
-					e.target
-						.closest('a')
-						.nextElementSibling.classList.add('hidden');
-				}
-			});
-		});
-	};
-
-	dropdownMenu();
-</script>
 
 <script type="text/javascript">
 
